733f18c5d3c231a74b6cfe036eeadd6125ead09e
leoleung2000 <leoleung2000@yahoo.com.hk>
2020-07-03 15:57:17 +0800

    Update delete in bst

--- binary_search_tree/bst.h
+++ binary_search_tree/bst.h
@@ -8,8 +8,15 @@ class Node {
             right = nullptr;
             value = input_value; }
         ~Node(){
-            if (left != nullptr) {delete left;}
-            if (right != nullptr) {delete right;}
+            //if (left != nullptr) {delete left; left = nullptr;}
+            //if (right != nullptr) {delete right; right = nullptr;}
+        }
+
+        void DebugPrint(){
+            std::cout << "Node Value : " << value << " ";
+            if (left != nullptr) std::cout << "Left Node Value : " << left->value << " ";
+            if (right != nullptr) std::cout << "Left Node Value : " << right->value << " ";
+            std::cout << std::endl;
         }
 
         int value;
@@ -60,6 +67,77 @@ class binary_Search_tree{
             return false;
         }
 
+        // 1) Node to be deleted is leaf: Simply remove from the tree.
+        /*
+                   50                            50
+                /     \         delete(20)      /   \
+                30      70       --------->    30     70 
+                /  \    /  \                     \    /  \ 
+              20   40  60   80                   40  60   80
+        */
+        // 2) Node to be deleted has only one child: Copy the child to the node and delete the child
+        /*
+                   50                             50
+                /     \         delete(30)      /   \
+                30      70       --------->    40     70 
+                  \    /  \                          /  \ 
+                  40  60   80                       60   80
+        */
+        // 3) Node to be deleted has two children: Find inorder successor of the node. 
+        // Copy contents of the inorder successor to the node and delete the inorder successor. 
+        // Note that inorder predecessor can also be used.
+        /*
+                   50                            60
+                /     \         delete(50)      /   \
+               40      70       --------->    40    70 
+                        /  \                            \ 
+                       60   80                           80
+        */
+        void remove(int value){
+            if (root == nullptr) return;
+            Node* currentNode = root;
+            Node* parentNode = nullptr;
+            while (currentNode != nullptr){
+                if (currentNode->value == value ) {
+                    if (currentNode->left == nullptr && currentNode->right == nullptr){ // Case 1
+                        if (parentNode->left->value == value) {delete currentNode; parentNode->left = nullptr;}
+                        if (parentNode->right->value == value) {delete currentNode; parentNode->right = nullptr;}
+                    } else if (currentNode->left != nullptr && currentNode->right != nullptr){ // Case 3, two child                         
+                        Node* minNode = minValueNode(currentNode->right);
+                        currentNode->value = minNode->value;
+                        delete minNode;
+                    }
+                    else { 
+                        if (currentNode->left != nullptr){  // Case 2 , one child (left)
+                            if (parentNode->left->value == value) {
+                                parentNode->left = currentNode->left;
+                                delete currentNode;
+                            } else {
+                                parentNode->right = currentNode->left;
+                                delete currentNode;
+                            }
+                        }  
+                        else if (currentNode->right != nullptr){  // Case 2 , one child (right)
+                            if (parentNode->left->value == value) {
+                                parentNode->left = currentNode->right;
+                                delete currentNode;
+                            } else {
+                                parentNode->right = currentNode->right;
+                                delete currentNode;
+                            }
+                        } 
+                    }
+                }
+                else if (value < currentNode->value) {
+                    parentNode = currentNode;
+                    currentNode = currentNode->left;
+                } else {
+                    parentNode = currentNode;
+                    currentNode = currentNode->right;
+                }
+            }
+        }
+
         void DebugPrint(){
             if (root == nullptr) return;
 
@@ -79,8 +157,18 @@ class binary_Search_tree{
                     processingNode.push(currentNode->right);
                 }
             }
+            std::cout << std::endl;
         }
     private: 
         Node* root;
+        Node* minValueNode(Node* node) 
+        { 
+            Node* current = node; 
+            /* loop down to find the leftmost leaf */
+            while (current && current->left != nullptr) {
+                current = current->left; 
+            }
+            return current; 
+        } 
 };
 
--- binary_search_tree/main.cpp
+++ binary_search_tree/main.cpp
@@ -1,22 +1,32 @@
 #include "bst.h"
 
-//     9
-//  4     20
-//1  6  15  170
+/* 
+        50          
+    /     \        
+    30      70     
+    /  \    /  \   
+    20   40  60   80  
+*/
 
 int main() {
     binary_Search_tree tree;
-    tree.insert(9);
-    tree.insert(4);
-    tree.insert(6);
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(70);
     tree.insert(20);
-    tree.insert(170);
-    tree.insert(15);
-    tree.insert(1);
-    if (tree.lookup(150)){
-        std::cout << "Element Found";
-    } else {
-        std::cout << "Not Found";
-    }
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(80);
+    std::cout << "Original Tree" << std::endl;
+    tree.DebugPrint();
+    std::cout << "Removing 20 (leaf)" << std::endl;
+    tree.remove(20); // Case 1
+    tree.DebugPrint();
+    std::cout << "Removing 30 (Have one child)" << std::endl;
+    tree.remove(30); // Case 2
+    tree.DebugPrint();
+    std::cout << "Removing 50 (Have two child)" << std::endl;
+    tree.remove(50); // Case 3
+    tree.DebugPrint();
     return 0;
 }
